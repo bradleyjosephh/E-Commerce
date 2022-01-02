a39250e109c782fdddac48eb9686523f6a4cd182
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-02 14:42:22 -0800

    category routes

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -3,26 +3,28 @@ const { Category, Product } = require('../models')
 
 // The `/api/categories` endpoint
 
-router.get('/categories', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/categories', async (req, res) => {
+  const categoryData = await Category.findAll({ include: [Product] })
+  res.json(categoryData)
 })
 
-router.get('/categories/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/categories/:id', async (req, res) => {
+  const categoryData = await Category.findOne({ where: { id: req.params.id }, include: [Product] })
+  res.json(categoryData)
 })
 
-router.post('/categories', (req, res) => {
-  // create a new category
+router.post('/categories', async (req, res) => {
+  const categoryData = await Category.create(req.body)
+  res.json(categoryData)
 })
 
 router.put('/categories/:id', (req, res) => {
   // update a category by its `id` value
 })
 
-router.delete('/categories/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/categories/:id', async (req, res) => {
+  await Category.destroy({ where: { id: req.params.id } })
+  res.sendStatus(200)
 })
 
 module.exports = router
